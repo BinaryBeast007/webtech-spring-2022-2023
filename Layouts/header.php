7f8355980678d43d19d47f6e0bc822a5d982db24
Abu Sayem <abusayem1299@gmail.com>
2023-03-10 20:58:31 +0600

    Added header and footer

---
+++ Layouts/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <table>
+        <th> <img src="http://localhost/WT%20Project/webtech-spring-2022-2023/Assets/rent-logo.png" width="50px" height="50px"> </th>
+        <th> Welcome to ABC House Rental Service </th>
+    </table>
+</body>
+</html>
