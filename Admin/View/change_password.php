1a1cd364f18640661a8ba3179275e844ce4ce2bf
Abu Sayem <abusayem1299@gmail.com>
2023-03-13 21:23:47 +0600

    Updated password change feature

--- Admin/View/change_password.php
+++ Admin/View/change_password.php
@@ -15,20 +15,26 @@
             <td rowspan="5">  
                 <fieldset>
                     <legend> Change Password </legend>
-                    <table>
-                        <tr>  
-                            <td>Current Password</td>
-                            <td><input type="text" name="current_pass" placeholder="Enter current password"></td>
-                        </tr>
-                        <tr>  
-                            <td>New Password</td>
-                            <td><input type="text" name="new_pass" placeholder="Enter new password"></td>
-                        </tr>
-                        <tr>  
-                            <td>Retype New Password</td>
-                            <td><input type="text" name="retype_pass" placeholder="Retype new password"></td>
-                        </tr>
-                    </table>
+                    <form action="../Controls/changePassword.php" method="POST" enctype="multipart/form-data">
+                        <table>
+                            <tr>  
+                                <td>Current Password</td>
+                                <td><input type="text" name="current_pass" placeholder="Enter current password"></td>
+                            </tr>
+                            <tr>  
+                                <td>New Password</td>
+                                <td><input type="text" name="new_pass" placeholder="Enter new password"></td>
+                            </tr>
+                            <tr>  
+                                <td>Retype New Password</td>
+                                <td><input type="text" name="retype_pass" placeholder="Retype new password"></td>
+                            </tr>
+                            <tr>
+                                <td> <input type="submit" value="Submit"> </td>
+                                <td> <input type="reset" value="Reset"> </td>
+                            </tr>
+                        </table>
+                    </form>
                 </fieldset>
             </td>
         </tr>
--- Admin/View/change_profile_picture.php
+++ Admin/View/change_profile_picture.php
@@ -8,32 +8,32 @@
 </head>
 <body>
     <?php include '../../Layouts/logged_in_header.php'; ?>
-    
-    <table border="1" style="border-collapse: collapse;" cellpadding="10">
-        <tr>
-            <td> <a href="dashboard.php"> Dashboard </a> </td>
-            <td rowspan="5"> 
-                <fieldset>
-                    <legend> Change Profile Picture </legend>
-                    <table>
-                        <tr>  
-                            <td>Current Picture</td>
-                            <td> <img src="<?php echo $current_user->file; ?>" alt="Profile Picture" width="50px" height="50px"> </td>
-                        </tr>
-                        <tr>  
-                            <td>New Picture</td>
-                            <td><input type="file" name="image"></td>
-                        </tr>
-                    </table>
-                </fieldset>
-            </td>
-        </tr>
-        <tr><td> <a href="view_profile.php"> View Profile </a> </td></tr>
-        <tr><td> <a href="edit_profile.php"> Edit Profile </a> </td></tr>
-        <tr><td> <a href="change_profile_picture.php"> Change Profile Picture </a> </td></tr>
-        <tr><td> <a href="change_password.php"> Change Password </a> </td></tr>
-    </table>
-
+    <form action="../Controls/changePassword.php" method="POST" enctype="multipart/form-data">
+        <table border="1" style="border-collapse: collapse;" cellpadding="10">
+            <tr>
+                <td> <a href="dashboard.php"> Dashboard </a> </td>
+                <td rowspan="5"> 
+                    <fieldset>
+                        <legend> Change Profile Picture </legend>
+                        <table>
+                            <tr>  
+                                <td>Current Picture</td>
+                                <td> <img src="<?php echo $current_user->file; ?>" alt="Profile Picture" width="50px" height="50px"> </td>
+                            </tr>
+                            <tr>  
+                                <td>New Picture</td>
+                                <td><input type="file" name="image"></td>
+                            </tr>
+                        </table>
+                    </fieldset>
+                </td>
+            </tr>
+            <tr><td> <a href="view_profile.php"> View Profile </a> </td></tr>
+            <tr><td> <a href="edit_profile.php"> Edit Profile </a> </td></tr>
+            <tr><td> <a href="change_profile_picture.php"> Change Profile Picture </a> </td></tr>
+            <tr><td> <a href="change_password.php"> Change Password </a> </td></tr>
+        </table>
+    </form>
     <?php include '../../Layouts/footer.php'; ?>
 </body>
 </html>
